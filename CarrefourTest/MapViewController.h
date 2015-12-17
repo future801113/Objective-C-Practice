a3562c96130638fc598af8b66dbff5607e1c0b9c
Jason <mac@MactekiMacBook-Pro.local>
2015-12-17 12:11:02 +0800

    MapView 定位

---
+++ CarrefourTest/MapViewController.h
@@ -0,0 +1,20 @@
+//
+//  MapViewController.h
+//  CarrefourTest
+//
+//  Created by Mac on 2015/12/16.
+//  Copyright © 2015年 Webcomm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+#import <MapKit/MKAnnotation.h>
+
+@interface MapViewController : UIViewController <MKMapViewDelegate,  CLLocationManagerDelegate>
+    
+- (IBAction)buttonClick:(id)sender;
+
+@property(nonatomic, retain) IBOutlet MKMapView *mapView;
+@property(nonatomic, retain) CLLocationManager *locationManager;
+
+@end
---
+++ CarrefourTest/MapViewController.m
@@ -0,0 +1,86 @@
+//
+//  MapViewController.m
+//  CarrefourTest
+//
+//  Created by Mac on 2015/12/16.
+//  Copyright © 2015年 Webcomm. All rights reserved.
+//
+
+#import "MapViewController.h"
+
+#define IS_OS_8_OR_LATER ([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0)
+
+@implementation MapViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.mapView.delegate = self;
+    self.locationManager = [[CLLocationManager alloc] init];
+    self.locationManager.delegate = self;
+
+    //要求權限
+#ifdef __IPHONE_8_0
+    if(IS_OS_8_OR_LATER) {
+        // Use one or the other, not both. Depending on what you put in info.plist
+        [self.locationManager requestWhenInUseAuthorization];
+        [self.locationManager requestAlwaysAuthorization];
+    }
+#endif
+    
+    [self.locationManager startUpdatingLocation];
+    
+    self.mapView.showsUserLocation = YES;
+    [self.mapView setMapType:MKMapTypeStandard];
+    [self.mapView setZoomEnabled:YES];
+    [self.mapView setScrollEnabled:YES];
+
+}
+
+-(void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:YES];
+    
+    
+    [self getUserLocation];
+}
+
+//更新位置
+- (void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation
+{
+    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(userLocation.coordinate, 800, 800);
+    [self.mapView setRegion:[self.mapView regionThatFits:region] animated:YES];
+}
+
+//取得經緯度
+- (NSString *)deviceLocation {
+    return [NSString stringWithFormat:@"latitude: %f longitude: %f", self.locationManager.location.coordinate.latitude, self.locationManager.location.coordinate.longitude];
+}
+
+//取得定位
+- (void) getUserLocation{
+    self.locationManager.distanceFilter = kCLDistanceFilterNone;
+    self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    [self.locationManager startUpdatingLocation];
+    NSLog(@"LOG: %@", [self deviceLocation]);
+    
+    //View Area
+    MKCoordinateRegion region = { { 0.0, 0.0 }, { 0.0, 0.0 } };
+    region.center.latitude = self.locationManager.location.coordinate.latitude;
+    region.center.longitude = self.locationManager.location.coordinate.longitude;
+    region.span.longitudeDelta = 0.005f;
+    region.span.longitudeDelta = 0.005f;
+    [self.mapView setRegion:region animated:YES];
+}
+
+- (IBAction)buttonLocate:(id)sender {
+    [self getUserLocation];
+}
+
+
+- (IBAction)buttonClick:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+
+
+@end
