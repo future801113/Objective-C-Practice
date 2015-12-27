3c5a7789adfc1f74b3a3227d078c889d5dec88c4
Jason <mac@MactekiMacBook-Pro.local>
2015-12-27 16:19:20 +0800

    推播頁面

---
+++ CarrefourTest/PushViewController.h
@@ -0,0 +1,15 @@
+//
+//  PushViewController.h
+//  CarrefourTest
+//
+//  Created by Mac on 2015/12/27.
+//  Copyright © 2015年 Webcomm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PushViewController : UIViewController
+
+- (IBAction)buttonClick:(id)sender;
+
+@end
---
+++ CarrefourTest/PushViewController.m
@@ -0,0 +1,15 @@
+//
+//  PushViewController.m
+//  CarrefourTest
+//
+//  Created by Mac on 2015/12/27.
+//  Copyright © 2015年 Webcomm. All rights reserved.
+//
+
+#import "PushViewController.h"
+
+@implementation PushViewController
+- (IBAction)buttonClick:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+@end
